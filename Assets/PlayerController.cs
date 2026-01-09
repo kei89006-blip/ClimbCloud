7d312c64289dd0641994cbd0e061c036c40a444b
kei89006-blip <kei89006@gmail.com>
2026-01-09 18:48:04 +0900

    add  horizontol movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,12 +5,15 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     float jumpForce= 600.0f;
+    float walkForce =300.0f;
+    float maxWalkSpeed =2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate =60;
         this.rigid2D =GetComponent<Rigidbody2D>();
     } // Update is called once per frame
+
     void Update()
     {
        //ジャンプする
@@ -18,6 +21,13 @@ public class PlayerController : MonoBehaviour
         {
             this.rigid2D.AddForce(transform.up*this.jumpForce);
         }
+
+        //右へ移動
+        if(this.rigid2D.linearVelocityX < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * walkForce);
+        }
+
         
     }
 }
