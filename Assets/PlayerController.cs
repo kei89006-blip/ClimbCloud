8535a2a6f5729f1f740c89de821fc65a4f4a8438
kei89006-blip <kei89006@gmail.com>
2026-01-12 12:26:11 +0900

    Add jumping logic and walk sprite switching

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
     float jumpForce= 600.0f;
     float walkForce =300.0f;
     float maxWalkSpeed =2.0f;
+    public Sprite[]walkSprites;
+    float time =0;
+    int idx =0;
+    SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate =60;
         this.rigid2D =GetComponent<Rigidbody2D>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     } // Update is called once per frame
 
     void Update()
@@ -26,6 +31,15 @@ public class PlayerController : MonoBehaviour
         if(this.rigid2D.linearVelocityX < this.maxWalkSpeed)
         {
             this.rigid2D.AddForce(transform.right * walkForce);
+        }
+
+        //アニメーション
+        this.time +=Time.deltaTime;
+        if(this.time > 0.1f)
+        {
+            this.time =0;
+            this.spriteRenderer.sprite =walkSprites [this.idx];
+            this.idx =1- this.idx;
         }
 
         
